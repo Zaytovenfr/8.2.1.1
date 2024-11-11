85056529c1f65054c26f98316d10f4353cbc4d2c
Zavaon Taylor <taylozav000@warren.k12.in.us>
2024-11-11 08:57:19 -0500

    s

---
+++ main.py
@@ -0,0 +1,17 @@
+color_list = ["red", "green", "blue", "yellow"]
+print(0)
+print(-1)
+
+num_list = [4, 6, 4, 2, 9, 4, 1]
+num_list.append(5)
+print(num_list)
+num_list.remove(4)
+print(num_list)
+num_list.count(4)
+num_list.index(9)
+print(num_list)
+
+fruit_list = ["apple", "banana", "cherry"]
+fruit_list[1] = "orange"
+fruit_list.pop(-1)
+print(fruit_list)
